1c7fb6b8de0757865353404821f0f946c6a3b57a
Tyler Sharp <johntylersharp@gmail.com>
2023-12-18 20:10:27 -0500

    Added project structure, sourcing in games from an object and enhancing the Marbles Scoring feature

--- components/Marbles.js
+++ components/Marbles.js
@@ -1,15 +1,30 @@
 class Marbles {
-  constructor() {
-    this.bruh = "bruh";
+  constructor(gameChannelNamesList) {
+    this.gameChannelNamesList = gameChannelNamesList;
   }
-  checkIfGameChannel() {}
-  checkWhichGame() {}
-  parseScore() {}
-  determineWinner() {}
-  giveMarble() {}
-  takeMarble() {}
-  addMarbleEmoji() {}
-  removeMarbleEmoji() {}
+  checkGameChannel(channelName) {
+    let check = 0;
+    let currentGame = "not a game channel";
+
+    for (let i = 0; i < this.gameChannelNamesList; i++) {
+      if (channelName == this.gameChannelNamesList[i]) {
+        check = 1;
+        currentGame = this.gameChannelNamesList[i];
+        break;
+      }
+    }
+
+    if (!check) return;
+
+    if (check) return currentGame;
+  }
+
+  parseScore(msg, gameName) {}
+  determineWinner(gameType, scoresArr) {}
+  giveMarble(user, game, gameNumber) {}
+  takeMarble(user, game, gameNumber) {}
+  addMarbleEmoji(user, msg) {}
+  removeMarbleEmoji(user, msg) {}
 }
 
 module.exports = {
---
+++ sources/games.js
@@ -0,0 +1,28 @@
+const games = [
+  {
+    name: "Guess the Game",
+    channelName: "guess-the-game",
+    scoringCriteria: "lowest",
+    scoringType: "guessingGame",
+  },
+  {
+    name: "Guess the Audio",
+    channelName: "guess-the-audio",
+    scoringCriteria: "lowest",
+    scoringType: "guessingGame",
+  },
+  {
+    name: "Wordle",
+    channelName: "wordle",
+    scoringCriteria: "lowest",
+    scoringType: "wordGame",
+  },
+  {
+    name: "Octordle",
+    channelName: "octordle",
+    scoringCriteria: "lowest",
+    scoringType: "wordGame",
+  },
+];
+
+module.exports = {};
